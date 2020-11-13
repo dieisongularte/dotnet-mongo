8e9c63f3633fefc9f50e3d92a96dc52f17432d03
Dieison <dieissongularte@gmail.com>
2020-11-13 17:08:31 -0300

    add PersonageController

---
+++ API/API/Controllers/PersonageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using API.Data.Collections;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonageController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Personage> _personagesCollection;
+
+        public PersonageController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _personagesCollection = _mongoDB.DB.GetCollection<Personage>(typeof(Personage).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarPersonage([FromBody] PersonageDto dto)
+        {
+            var personage = new Personage(dto.Nome, dto.Sexo, dto.DataNascimento, dto.Latitude, dto.Longitude);
+            _personagesCollection.InsertOne(personage);
+            return StatusCode(201, "Personage adicinado com sucesso.");
+        }
+
+        [HttpGet]
+        public ActionResult ObterPersonages()
+        {
+            var personages = _personagesCollection.Find(Builders<Personage>.Filter.Empty).ToList();
+            return Ok(personages);
+        }
+    }
+}
