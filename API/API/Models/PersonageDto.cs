4954956baed6079d612904909a2cacb263ce713e
Dieison <dieissongularte@gmail.com>
2020-11-13 15:51:41 -0300

    Add class PersonageDto

---
+++ API/API/Models/PersonageDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace API.Models
+{
+    public class PersonageDto
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
