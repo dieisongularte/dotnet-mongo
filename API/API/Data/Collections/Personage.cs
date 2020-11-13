8e27b5a427adbe4575239072a7b3e146f7d59269
Dieison <dieissongularte@gmail.com>
2020-11-13 15:14:32 -0300

    update class personage

--- API/API/Data/Collections/Personage.cs
+++ API/API/Data/Collections/Personage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MongoDB.Driver.GeoJsonObjectModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,17 @@ namespace API.Data.Collections
 {
     public class Personage
     {
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
 
+        public Personage(string nome, string sexo, DateTime dataNascimento, double latitude, double longitude)
+        {
+            Nome = nome;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+            Localizacao = new GeoJson2DGeographicCoordinates(latitude, longitude);
+        }
     }
 }
