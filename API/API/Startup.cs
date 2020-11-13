e24b19c3b17672883f3ea07c7d1d9a6bc420f876
Dieison <dieissongularte@gmail.com>
2020-11-13 12:02:08 -0300

    inicia configurações

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -30,14 +30,9 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
